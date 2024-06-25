ef2a9e8eb7037ff61056955977747385d69ca06b
Mohamed Langi <mlangi@stlhack.com>
2024-06-25 10:44:10 -0500

    test

---
+++ main.js
@@ -0,0 +1,45 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+import { ScreenNode } from "three/examples/jsm/nodes/display/BlendModeNode.js";
+
+//scene
+const scene = new THREE.Scene();
+
+//Create a sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({
+  color: "#00ff83",
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+//Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+//Light
+const light = new THREE.PointLight(0xffffff, 200, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
+
+//Camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height);
+camera.position.z = 20;
+scene.add(camera);
+
+//Render scene
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+const controls = new OrbitControls(camera, canvas);
+
+const loop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+loop();
